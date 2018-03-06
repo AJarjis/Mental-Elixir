23e7cea6fcbea5ea2a2e0dec42f2d90a777681c1
Ali Jarjis <AJarjis@hotmail.co.uk>
2018-03-06 12:49:03 +0000

    Classes Template Created
    Small steps but have created empty files for all the classes needed for the model. This commit is primarily to test using git and for demonstrating it.

---
+++ Elixir/src/java/Model/Mood.java
@@ -0,0 +1,21 @@
+package Model;
+
+/*****************************************************************************
+
+File        : Mood.java
+
+Date        : 06-Mar-2018
+
+Description : A class 
+
+Author      : Ali Jarjis
+
+******************************************************************************/
+
+/**
+ *
+ * @author Ali Jarjis
+ */
+public class Mood {
+
+}
---
+++ Elixir/src/java/Model/User.java
@@ -0,0 +1,21 @@
+package Model;
+
+/*****************************************************************************
+
+File        : User.java
+
+Date        : 06-Mar-2018
+
+Description : A class 
+
+Author      : Ali Jarjis
+
+******************************************************************************/
+
+/**
+ *
+ * @author Ali Jarjis
+ */
+public class User {
+
+}
