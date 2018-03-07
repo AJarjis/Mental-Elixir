56e7a23120d32a3821b0ea359d195227c296f2f6
Ashley Black <A.Black@uea.ac.uk>
2018-03-07 13:30:25 +0000

    Assessment Class Basic Implementation Done
    Variables, constructor and methods added for Assessment class.

--- Elixir/src/java/Model/Assessment.java
+++ Elixir/src/java/Model/Assessment.java
@@ -1,21 +1,78 @@
 package Model;
 
-/*****************************************************************************
-
-File        : Assessment.java
-
-Date        : 06-Mar-2018
-
-Description : A class 
-
-Author      : Ashley Black
-
-******************************************************************************/
+import java.util.Date;
 
 /**
+ * ***************************************************************************
  *
- * @author Ashley Black
+ * File : Assessment.java
+ *
+ * Date : 06-Mar-2018
+ *
+ * Description : Code for class Assessment, based on the WEMWBS assessment a
+ * user takes on the Mental Health Application.
+ *
+ * Author : Ashley Black
+ *
+ *****************************************************************************
  */
 public class Assessment {
+
+    /**
+     * score: the result a user receives once they have finished taking the
+     * WEMWBS assessment.
+     */
+    private int score;
+
+    /**
+     * date: the date that a user takes the WEMWBS assessment.
+     */
+    private Date date;
+
+    /**
+     * @description constructor for the Assessment class.
+     *
+     * @param score
+     * @param date
+     *
+     */
+    public Assessment(int score, Date date) {
+
+        this.score = score;
+        this.date = date;
+
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+
+        return score;
+    }
+
+    /**
+     * @param score the score to set
+     */
+    public void setScore(int score) {
+
+        this.score = score;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+
+        this.date = date;
+    }
 
 }
