72709d8136606dbf968772b815cace524f22cd38
Daniel Carey <hyv15asu@uea.ac.uk>
2018-03-07 14:50:46 +0000

    stuff for Keiran

--- Elixir/src/java/Model/Profile.java
+++ Elixir/src/java/Model/Profile.java
@@ -1,21 +1,70 @@
 package Model;
 
+import java.util.List;
+
 /*****************************************************************************
 
 File        : User.java
 
 Date        : 06-Mar-2018
 
-Description : A class 
+Description : This class represents the core of the backend. Goals, mood
+              ,groups and assessments will be manipulated through this class.
 
-Author      : Ali Jarjis
+Author      : Daniel Carey
 
 ******************************************************************************/
 
-/**
- *
- * @author Ali Jarjis
- */
-public class User {
+public class Profile {
+    private User user;
+    private List<Goal> goal;
+    private List<Mood> mood;
+    private List<Assessment> assessment;
+    private List<Group> group;
+
+    /**
+     * @return the goal
+     */
+    public List<Goal> getGoal() {
+        return goal;
+    }
+
+    /**
+     * @return the mood
+     */
+    public List<Mood> getMood() {
+        return mood;
+    }
+
+    /**
+     * @return the assessment
+     */
+    public List<Assessment> getAssessment() {
+        return assessment;
+    }
+
+    /**
+     * @return the group
+     */
+    public List<Group> getGroup() {
+        return group;
+    }
+    /**
+     * Method to add a goal to the list of goals
+     * @param goal object you wish to add to the list
+     */
+    public void addGoal(Goal goal){
+        this.goal.add(goal);
+    }
+    
+    /**
+     * Method to remove goal specified. Truth 
+     * @param goal
+     * @return 
+     */
+    public boolean removeGoal(Goal goal){
+       return this.goal.remove(goal);
+    }
+    
 
 }
--- Elixir/src/java/Model/User.java
+++ Elixir/src/java/Model/User.java
@@ -6,16 +6,110 @@ File        : Account.java
 
 Date        : 06-Mar-2018
 
-Description : A class 
+Description : User class will be in charge of managing the user fields.
 
-Author      : Ali Jarjis
+Author      : Daniel Carey
 
 ******************************************************************************/
 
-/**
- *
- * @author Ali Jarjis
- */
-public class Account {
+
+public class User {
+    
+    private String userName;
+    private String firstName;
+    private String surname;
+    private String email;
+    private String password;
+
+    /**
+     * Creates a new new user object in the system
+     * @param userName
+     * @param firstName
+     * @param surname
+     * @param email
+     * @param password 
+     */
+    public User(String userName, String firstName, String surname, String email, String password) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+    
+    
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
 
 }
