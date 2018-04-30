265b4165fa505483d5f8268dfa7e2eb296fd46a2
Ashley Black <A.Black@uea.ac.uk>
2018-04-30 11:29:48 +0100

    My 4 controllers complete. 2 static methods need work.

--- Elixir/src/java/Controller/AssessmentController.java
+++ Elixir/src/java/Controller/AssessmentController.java
@@ -4,18 +4,61 @@ File        : AssessmentController.java
 
 Date        : 11-Mar-2018
 
-Description : A class 
+Description : A class for dealing with the mental health assessment that a user
+              can partipate in on their profile.
 
-Author      : Ali Jarjis
+Author      : Ashley Black
 
 ******************************************************************************/
 
 package Controller;
 
-/**
- *
- * @author Ali Jarjis
- */
+import Model.Assessment;
+import java.util.Date;
+
 public class AssessmentController {
+    
+    private Assessment assessment;
+    
+    public AssessmentController(Assessment assessment) {
+        
+        this.assessment = assessment;
+    }
+    
+    /**
+     * @return the assessment score
+     */
+    public int getScore() {
+        
+        return assessment.getScore();
+    }
+    
+    /**
+     * @return the assessment date
+     */
+    public Date getDate() {
+        
+        return assessment.getDate();
+    }
+    
+    /**
+     * @param score
+     * 
+     * Description: Sets the score for the given assessment in question
+     */
+    public void setScore(int score) {
+        
+        this.assessment.setScore(score);
+    } 
+    
+    /**
+     * @param score
+     * 
+     * Description: Sets the date for the given assessment in question
+     */
+    public void setDate(Date date) {
+        
+        this.assessment.setDate(date);
+    }
 
 }
