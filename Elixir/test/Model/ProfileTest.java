d6a89c16527dcc84927d1bf2d188f80de15d5b40
Daniel Carey <hyv15asu@uea.ac.uk>
2018-05-22 22:02:58 +0100

    Auto stash before merge of "danielDev" and "origin/master"

--- Elixir/test/Model/ProfileTest.java
+++ Elixir/test/Model/ProfileTest.java
@@ -270,8 +270,8 @@ public class ProfileTest {
         actList2.add(act22);
         Calendar date2 = Calendar.getInstance();
         date2.set(2018, 06, 06, 5, 15, 10);
-        Goal g2 = new Goal(actList2, date2);
-        goals.add(g1);
+        Goal g3 = new Goal(actList2, date2);
+        goals.add(g3);
         goals.add(g2);
         List<Mood> moods = new LinkedList<>();
         List<Assessment> asmts = new LinkedList<>();
